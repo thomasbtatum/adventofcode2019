ccc3ed69a3a45d56329db5ac186a4451c868f62a
Thomas Tatum <thomasbtatum@users.noreply.github.com>
2020-01-11 13:47:22 +0100

    work in progress

--- AC2019/Day14.py
+++ AC2019/Day14.py
@@ -1,23 +1,43 @@
 f = open("AC2019/Day14.0")
 s = f.readlines()
 
-def findInputChem(outChem, dict):
-    for k,v in dict.items():
+def findInputChem(outChem, rdict):
+    for k,v in rdict.items():
         if k[1] == outChem:
             return v
 
-def getOre(count,outChem,dict):
-    inList = findInputChem(outChem, dict)
+def getOreInputCnt(outChem, rdict):
+    inList = findInputChem(outChem, rdict)
     if len(inList) == 1 and inList[0][1] == 'ORE':
-        return count * int(inList[0][0])
+        return inList[0][0]
     else:
-        total = 0
-        for inChem in inList:
-            total += (int(inChem[0]) // count) * int(getOre(int(inChem[0]),inChem[1],dict))
-        return total
+        return -1
+
+def buildreduxList(chem, reduxList, rdict, oreList):
+    inChemList = findInputChem(chem,rdict)
+    for inChem in inChemList:
+        cnt = getOreInputCnt(inChem[1],rdict)
+        hasOreIn = False if cnt == -1 else True
+        ic = tuple([inChem[0],tuple([inChem[1],hasOreIn])])
+        if hasOreIn:
+            addOrrList(ic, oreList)
+        else:
+            reduxList.append(ic)
+
+def addOrrList(chem, oreList):
+    #if chem exist, add, else append
+    for o in oreList:
+        if o[1][0] == chem[1][0]:
+            o[0] += int(chem[0])
+            return
+    newOre = [int(chem[0]),chem[1][0]]
+    oreList.append(newOre)
 
 reactions = {}
-totalFuel = 0
+totalOre = 0
+reduxList = []
+oreList = []
+
 for line in s:
     rewords = line.split('=>')
     leftSplit = rewords[0].strip().split(',')
@@ -28,12 +48,40 @@ for line in s:
     for val in leftSplit:
         lword = val.strip().split(' ')
         tupList.append(tuple([lword[0],lword[1]]))
-
+    assert key not in reactions
     reactions[key] = tupList
+print(reactions)
 
-totalFuel = getOre(1,'FUEL',reactions) 
+#phase 0 - build reduxList with 1 FUEL inputs
+#list is tuple(cnt,tuple(chem,hasOreIn))
+buildreduxList('FUEL',reduxList, reactions, oreList)
+print(reduxList, oreList)
+# phase 1 - while list has chems that dont have ore input
+# replace with input chems * count, adding any new ones that 
+# have ore input to oreList
+while len(reduxList) > 0:
+    f = reduxList.pop(0)
+    inChemList = findInputChem(f[1][0],reactions)
+    for inChem in inChemList:
+        cnt = getOreInputCnt(inChem[1],reactions)
+        hasOreIn = False if cnt == -1 else True
+        ic = tuple([inChem[0],tuple([inChem[1],hasOreIn])])
+        if hasOreIn:
+            addOrrList(ic,oreList)
+        else:
+            reduxList.append(ic)
+    print(reduxList, oreList)
+
+while len(oreList) > 0:
+    o = oreList.pop(0)
+    cnt = getOreInputCnt(o[1][0],reactions)
+    totalOre += int(cnt) * int(o[0])
+    print(oreList, totalOre)
+
+
+# phase 2 add up ore to get count
 
 #11 RVCS => 8 CBMDT
 #29 QXPB, 8 QRGRH => 8 LGMKD
 #3 VPRVD => 6 PMFZG
-print(reactions, totalFuel)
+#print(reactions, totalFuel)
